a1ba0b17a938db3d426508c8d58c3ec856fbf955
Wendinoda Washaya <wendinoda@Wendinodas-MacBook-Pro.local>
2022-11-25 15:09:34 +0200

    added UI and remote config values

--- app/src/main/java/com/tutorial/abtesting/MainActivity.kt
+++ app/src/main/java/com/tutorial/abtesting/MainActivity.kt
@@ -2,10 +2,27 @@ package com.tutorial.abtesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.installations.FirebaseInstallations
+import com.tutorial.abtesting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        FirebaseInstallations.getInstance().getToken(true)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("Installations", "Installation auth token: " + task.result?.token)
+                } else {
+                    Log.e("Installations", "Unable to get Installation auth token")
+                }
+            }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
     }
 }
---
+++ app/src/main/java/com/tutorial/abtesting/RemoteConfigValues.kt
@@ -0,0 +1,7 @@
+package com.tutorial.abtesting
+
+object RemoteConfigValues {
+    const val HELLO_USER = "Hello_user"
+    const val BACKGROUND_COLOR = "background_color"
+    const val SELECTED_COLOR = "selected_color"
+}
